d31e872f0045b7edfa978085059855e620af2888
Viques <vincenzo.bova05@gmail.com>
2023-04-11 14:56:48 +0200

    .

--- app/src/main/java/it/viquess/hypercane/MainActivity.java
+++ app/src/main/java/it/viquess/hypercane/MainActivity.java
@@ -2,13 +2,23 @@ package it.viquess.hypercane;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView bt_connected;
+    private BluetoothAdapter BA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_connected = findViewById(R.id.bt_connection);
+
+        //https://www.youtube.com/watch?v=iFtjox9_zAI
+        //https://www.youtube.com/watch?v=TtpLcsQ4nMw
     }
 }
